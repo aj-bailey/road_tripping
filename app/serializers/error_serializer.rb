29aeac3a1394bada30f476d72a87adf8c7909b37
Adam Bailey <ab67319@gmail.com>
2023-04-22 15:57:06 -0600

    feat: create error serializer

---
+++ app/serializers/error_serializer.rb
@@ -0,0 +1,32 @@
+class ErrorSerializer
+  def initialize(exception)
+    @exception = exception
+  end
+
+  def errors
+    {
+      "message": error_message,
+      "errors": parse_error_message
+    }
+  end
+
+  def error_message
+    if @exception.message.include?(":")
+      @exception.message.split(":")[0]
+    else
+      "your query could not be completed"
+    end
+  end
+
+  def parse_error_message
+    return [@exception.message] unless @exception.message.include?(":")
+
+    colon_index = @exception.message.index(":")
+    @exception.message[(colon_index + 2)..-1].split(", ")
+  end
+
+
+  def status
+    return 400 if @exception.class == ActiveRecord::RecordInvalid
+  end
+end
