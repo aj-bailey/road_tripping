8c15c01a52e9f55e8e88705e26372ded6f5a54e7
Adam Bailey <ab67319@gmail.com>
2023-04-22 12:02:10 -0600

    test: create service, poros, facades tests

---
+++ spec/services/map_quest_service_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe MapQuestService do
+  describe "#instance methods" do
+    let(:service) { MapQuestService.new("denver,co")}
+    let(:location) { VCR.use_cassette("MapQuest Service Denver") { service.get_location } }
+
+    describe "initialize" do
+      it "exists" do
+        expect(service).to be_a(MapQuestService)
+      end
+    end
+
+    describe "get_location" do
+      it "returns details for map quest location" do
+        expect(location).to be_a(Hash)
+        expect(location[:results][0][:locations][0][:latLng]).to be_a(Hash)
+        expect(location[:results][0][:locations][0][:latLng].keys).to contain_exactly(:lat, :lng)
+      end
+    end
+  end
+end
