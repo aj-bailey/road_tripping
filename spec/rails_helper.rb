4a304c0f1e431aa2d4d02cc0834ffc2bfeacae76
Adam Bailey <ab67319@gmail.com>
2023-04-22 11:14:39 -0600

    refactor: update VCR to filter api keys

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -75,5 +75,7 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  config.filter_sensitive_data('<PROPUBLICA_API_KEY>') { ENV["PROPUBLICA_API_KEY"] }
+  config.filter_sensitive_data('<WEATHER_API_KEY>') { ENV["WEATHER_API_KEY"] }
+  config.filter_sensitive_data('<MAP_QUEST_API_KEY>') { ENV["MAP_QUEST_API_KEY"] }
+  config.configure_rspec_metadata!
 end
