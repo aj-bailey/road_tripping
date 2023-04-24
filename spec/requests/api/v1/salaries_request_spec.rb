225ac740387feae2e45fa852b51f7ed12668a2fb
Adam Bailey <ab67319@gmail.com>
2023-04-24 09:17:20 -0600

    test: create salaries request spec file

---
+++ spec/requests/api/v1/salaries_request_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe "Salaries API Request" do
+  describe "Salaries Create" do
+    context "Successful Request" do
+
+    end
+
+    context "Unsuccessful Request" do
+
+    end
+  end
+end
